ba1a56b45d1cfd002abafab390640ed3a5a11374
hugosenari <hugosenari@gmail.com>
2022-03-12 04:11:11 -0300

    chore: re index sksbox

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A native-nim implementaton of the sBOX generic container format.'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-sksbox-master.flake = false;
+  inputs.src-sksbox-master.owner = "Skrylar";
+  inputs.src-sksbox-master.ref   = "refs/heads/master";
+  inputs.src-sksbox-master.repo  = "sksbox";
+  inputs.src-sksbox-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-sksbox-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-sksbox-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
